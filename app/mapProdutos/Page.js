180a83af7ae8db69f5d84caf629dbc1065e58676
Leonardo Henrique Mazza <141638709+LeonardoHMazza@users.noreply.github.com>
2025-03-17 20:19:02 -0300

    Add files via upload

---
+++ app/mapProdutos/Page.js
@@ -0,0 +1,12 @@
+import listaProdutos from "@/data/listaProdutos";
+
+export default function TabelaDeProdutos(){
+    
+
+    return (
+        <div>
+            <h1>Tabela de Produtos</h1>
+
+        </div>
+    )
+}
